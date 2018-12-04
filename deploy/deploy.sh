665f9b91df7403d706edb7c61c57a159c1354301
sreekar <sreekar339@gmail.com>
2018-12-04 15:39:20 +0530

    added homepage field, change in updateRepo.sh

---
+++ deploy/deploy.sh
@@ -0,0 +1,2 @@
+echo "deploying to 139.59.93.218 - sreekar.co"
+ssh sreekar339@139.59.93.218 'bash' < ./deploy/updateRepo.sh
---
+++ deploy/updateRepo.sh
@@ -0,0 +1,6 @@
+set -e
+
+rm -rf rm -rf ~/xoxo-multiplayer/xoxo-multiplayer-frontend
+
+scp build sreekar339@139.59.93.218:~/xoxo-multiplayer/xoxo-multiplayer-frontend
+
